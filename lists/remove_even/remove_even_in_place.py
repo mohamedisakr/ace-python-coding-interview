23b9787eae3683e7b36bc964bb7171fcd32e53e0
mohamedisakr <mohamedisakr@gmail.com>
2024-10-03 17:53:54 +0300

    remove even delete redundant code

--- lists/remove_even/remove_even_in_place.py
+++ lists/remove_even/remove_even_in_place.py
@@ -8,26 +8,3 @@ def remove_even(lst):
         if not isinstance(x, (int, float)):
             raise TypeError("List contains invalid elements")
     return [x for x in lst if isinstance(x, (int, float)) and x % 2 != 0]
-
-
-'''
-import math
-
-
-def remove_even(lst):
-    # if not all(isinstance(x, (int, float)) for x in lst):
-    if not all(isinstance(x, (int, float)) for x in lst):
-        raise TypeError("List contains invalid elements")
-    return [x for x in lst if isinstance(x, (int, float)) and x % 2 != 0]
-'''
-
-'''
-import math
-
-
-def remove_even(lst):
-    for x in lst:
-        if not isinstance(x, (int, float)) or (isinstance(x, float) and math.isnan(x)):
-            raise TypeError("List contains invalid elements")
-    return [x for x in lst if isinstance(x, (int, float)) and x % 2 != 0]
-'''
