66b5649abe82ab77387c2b7ff739e3c81599c4b7
mohamedisakr <mohamedisakr@gmail.com>
2024-10-04 06:42:51 +0300

    merge lists backward & test cases

---
+++ lists/merge_2_sorted/merge_2_sorted_list_backward.py
@@ -0,0 +1,34 @@
+def merge_lists(lst1, lst2):
+    m = len(lst1)
+    n = len(lst2)
+    arr = [0] * (m + n)
+
+    if m == 0:
+        return lst2
+    if n == 0:
+        return lst1
+
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+
+    while i >= 0 and j >= 0:
+        if lst1[i] > lst2[j]:
+            arr[k] = lst1[i]
+            i -= 1
+        else:
+            arr[k] = lst2[j]
+            j -= 1
+        k -= 1
+
+    while i >= 0:
+        arr[k] = lst1[i]
+        i -= 1
+        k -= 1
+
+    while j >= 0:
+        arr[k] = lst2[j]
+        j -= 1
+        k -= 1
+
+    return arr
