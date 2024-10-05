024248a278967f9c8f57e527015f1fc6f24305ca
mohamedisakr <mohamedisakr@gmail.com>
2024-10-05 10:18:42 +0300

    list of products of all elements optimize multiplication

--- lists/list_products_all_elements/list_products_all_elements.py
+++ lists/list_products_all_elements/list_products_all_elements.py
@@ -1,55 +1,23 @@
 
 def find_product(lst):
     n = len(lst)
-    if n == 0:
-        return []
+    product = []
 
-    left_products = [1] * n
-    right_products = [1] * n
-    result = [1] * n
+    # get product starting from left
+    left = 1
 
-    # Fill left_products
-    for i in range(1, n):
-        left_products[i] = left_products[i - 1] * lst[i - 1]
+    for item in lst:
+        product.append(left)
+        left = left * item
 
-    # Fill right_products
-    for i in range(n - 2, -1, -1):
-        right_products[i] = right_products[i + 1] * lst[i + 1]
+    # get product starting from right
+    right = 1
 
-    # Fill result
-    for i in range(n):
-        result[i] = left_products[i] * right_products[i]
+    for i in range(n-1, -1, -1):
+        product[i] = product[i] * right
+        right = right * lst[i]
 
-    return result
+    return product
 
 
-arr = [1, 2, 3, 4]
-# index = 0
-# total = prod(arr, index)
-# print(f'array product without index # {index} is {total}')
-
-res = find_product(arr)
-print(res)
-# print(f'arr length is: {len(res)}')
-
-arr1 = [4, 2, 1, 5, 0]
-res1 = find_product(arr1)
-print(res1)
-
-'''
-def prod(arr, index):
-    total = 1
-    n = len(arr)
-    for i in range(n):
-        if i != index:
-            total *= arr[i]
-    return total
-
-
-def find_product(lst):
-    n = len(lst)
-    mul_list = []
-    for i in range(n):
-        mul_list.append(prod(lst, i))
-    return mul_list
-'''
+# print(find_product([1, 2, 3, 4]))
--- lists/list_products_all_elements/test_list_products_all_elements.py
+++ lists/list_products_all_elements/test_list_products_all_elements.py
@@ -3,6 +3,7 @@ from .list_products_all_elements import find_product
 
 
 class TestFindProduct(TestCase):
+
     def test_general_case(self):
         self.assertEqual(find_product([1, 2, 3, 4]), [24, 12, 8, 6])
         self.assertEqual(find_product([2, -3, 4, -5]), [60, -40, 30, -24])
@@ -29,6 +30,36 @@ class TestFindProduct(TestCase):
 
     def test_empty_list(self):
         self.assertEqual(find_product([]), [])
+
+    # --------- other test cases ---------
+
+    def test_repeated_elements(self):
+        self.assertEqual(find_product([2, 2, 2, 2]), [8, 8, 8, 8])
+
+    def test_alternating_positive_negative(self):
+        self.assertEqual(find_product([1, -1, 1, -1]), [1, -1, 1, -1])
+
+    def test_large_list_size(self):
+        large_list = list(range(1, 1001))
+        expected_output = [1] * 1000
+        product_all = 1
+        for num in large_list:
+            product_all *= num
+        for i in range(1000):
+            expected_output[i] = product_all // large_list[i]
+        self.assertEqual(find_product(large_list), expected_output)
+
+    def test_floating_point_numbers(self):
+        self.assertEqual(find_product([1.5, 2.5, 3.5, 4.5]), [
+                         39.375, 23.625, 16.875, 13.125])
+
+    def test_mixed_integers_floats(self):
+        self.assertEqual(find_product([1, 2.5, 3, 4.5]), [
+                         33.75, 13.5, 11.25, 7.5])
+
+    def test_large_small_numbers(self):
+        self.assertEqual(find_product(
+            [1e10, 1e-10, 1e10, 1e-10]), [1e-10, 1e10, 1e-10, 1e10])
 
 
 if __name__ == "__main__":
