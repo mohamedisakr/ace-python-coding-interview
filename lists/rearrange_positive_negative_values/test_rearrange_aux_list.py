202ea4331a7e8d476965a3e4d6ec5b0d4d1ce3e1
mohamedisakr <mohamedisakr@gmail.com>
2024-10-06 21:53:41 +0300

    rearrange positive & negative aux list

---
+++ lists/rearrange_positive_negative_values/test_rearrange_aux_list.py
@@ -0,0 +1,46 @@
+from unittest import TestCase, main
+from .rearrange_aux_list import rearrange
+
+
+class TestRearrangeFunction(TestCase):
+
+    def test_all_positive(self):
+        self.assertEqual(rearrange([1, 2, 3, 4]), [1, 2, 3, 4])
+
+    def test_all_negative(self):
+        self.assertEqual(rearrange([-1, -2, -3, -4]), [-1, -2, -3, -4])
+
+    def test_mixed_values(self):
+        self.assertEqual(
+            rearrange([10, -1, 20, 4, 5, -9, -6]), [-1, -9, -6, 10, 20, 4, 5])
+
+    def test_single_positive(self):
+        self.assertEqual(rearrange([1]), [1])
+
+    def test_single_negative(self):
+        self.assertEqual(rearrange([-1]), [-1])
+
+    def test_empty_list(self):
+        self.assertEqual(rearrange([]), [])
+
+    def test_zero_in_list(self):
+        self.assertEqual(rearrange([0, -1, 2, -3, 4]), [-1, -3, 0, 2, 4])
+
+    def test_large_numbers(self):
+        self.assertEqual(rearrange(
+            [1000000, -1000000, 500000, -500000]), [-1000000, -500000, 1000000, 500000])
+
+    def test_duplicates(self):
+        self.assertEqual(rearrange([1, -1, 1, -1]), [-1, -1, 1, 1])
+
+    def test_already_sorted(self):
+        self.assertEqual(
+            rearrange([-3, -2, -1, 1, 2, 3]), [-3, -2, -1, 1, 2, 3])
+
+    def test_reverse_sorted(self):
+        self.assertEqual(
+            rearrange([3, 2, 1, -1, -2, -3]), [-1, -2, -3, 3, 2, 1])
+
+
+if __name__ == '__main__':
+    main()
