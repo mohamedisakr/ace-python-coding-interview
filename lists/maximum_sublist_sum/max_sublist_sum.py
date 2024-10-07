3746080aabcccf38ac65e3220b83f45197a71968
mohamedisakr <mohamedisakr@gmail.com>
2024-10-07 22:24:34 +0300

    max sublist sum Kadane's Algorithm

---
+++ lists/maximum_sublist_sum/max_sublist_sum.py
@@ -0,0 +1,15 @@
+def find_max_sum_sublist(arr):
+    max_current = arr[0]
+    max_global = arr[0]
+    for item in arr[1:]:
+        max_current = max(item, item + max_current)
+        max_global = max(max_current, max_global)
+    return max_global
+
+
+# Example usage:
+array = [-2, -3, 4, -1, -2, 1, 5, -3]
+print(find_max_sum_sublist(array))  # Output: 7
+
+arr = [-4, 2, -5, 1, 2, 3, 6, -5, 1]
+print(find_max_sum_sublist(arr))  # Output: 12
