acc8e0df4b03f0a684f886c9bd369ebaf5b0cd80
mohamedisakr <mohamedisakr@gmail.com>
2024-10-11 17:50:38 +0300

    fix hide issue

--- stacks/stack.py
+++ stacks/stack.py
@@ -8,20 +8,20 @@ class MyStack:
 
     def push(self, value):
         self.stack_list.append(value)
-        self.size += 1
+        self.stack_size += 1
 
     def pop(self):
         if self.is_empty():
             return None
         self.stack_list.pop()
-        self.size -= 1
+        self.stack_size -= 1
 
     def peek(self):
         if self.is_empty():
             return None
         return self.stack_list[-1]
 
-    def size(self) -> int:
+    def capacity(self) -> int:
         return self.stack_size
 
 
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     stack_obj = MyStack()
     print(f'is_empty: {stack_obj.is_empty()}')
     print(f'peek: {stack_obj.peek()}')
-    print(f'size: {stack_obj.size()}')
+    print(f'size: {stack_obj.capacity()}')
