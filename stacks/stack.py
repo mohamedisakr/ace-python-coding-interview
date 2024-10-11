aa9256daa48a693cbc563ddd1d22be97ea6148fd
mohamedisakr <mohamedisakr@gmail.com>
2024-10-11 17:47:09 +0300

    stack data structure using list

---
+++ stacks/stack.py
@@ -0,0 +1,32 @@
+class MyStack:
+    def __init__(self):
+        self.stack_list = []
+        self.stack_size = 0
+
+    def is_empty(self) -> bool:
+        return self.stack_size == 0
+
+    def push(self, value):
+        self.stack_list.append(value)
+        self.size += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        self.stack_list.pop()
+        self.size -= 1
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.stack_list[-1]
+
+    def size(self) -> int:
+        return self.stack_size
+
+
+if __name__ == "__main__":
+    stack_obj = MyStack()
+    print(f'is_empty: {stack_obj.is_empty()}')
+    print(f'peek: {stack_obj.peek()}')
+    print(f'size: {stack_obj.size()}')
