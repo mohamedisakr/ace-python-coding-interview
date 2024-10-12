3f0948ede66856cabef687e14eb90d6e9298554e
mohamedisakr <mohamedisakr@gmail.com>
2024-10-12 11:42:10 +0300

    doubly linked list insert test cases

--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -6,6 +6,7 @@ class TestDoublyLinkedList(unittest.TestCase):
     def setUp(self):
         self.dll = DoublyLinkedList()
 
+    # ----- insert tail ------
     def test_empty_list(self):
         # Insert into an empty list
         self.dll.insert_at_tail(10)
@@ -45,6 +46,49 @@ class TestDoublyLinkedList(unittest.TestCase):
             self.assertEqual(current.data, value)
             current = current.next
         self.assertEqual(self.dll.tail.data, 50)
+        self.assertEqual(self.dll.count, 5)
+
+    # -------- insert head --------
+
+    def test_empty_list_head(self):
+        # Insert into an empty list
+        self.dll.insert_at_head(10)
+        self.assertEqual(self.dll.head.data, 10)
+        self.assertEqual(self.dll.tail.data, 10)
+        self.assertEqual(self.dll.head.next, None)
+        self.assertEqual(self.dll.tail.prev, None)
+
+    def test_single_node_list_head(self):
+        # Insert into a list with one node
+        self.dll.insert_at_head(20)
+        self.dll.insert_at_head(10)
+        self.assertEqual(self.dll.head.data, 10)
+        self.assertEqual(self.dll.tail.data, 20)
+        self.assertEqual(self.dll.head.next.data, 20)
+        self.assertEqual(self.dll.tail.prev.data, 10)
+
+    def test_multiple_node_list_head(self):
+        # Insert into a list with multiple nodes
+        self.dll.insert_at_head(30)
+        self.dll.insert_at_head(20)
+        self.dll.insert_at_head(10)
+        self.assertEqual(self.dll.head.data, 10)
+        self.assertEqual(self.dll.tail.data, 30)
+        self.assertEqual(self.dll.head.next.data, 20)
+        self.assertEqual(self.dll.tail.prev.data, 20)
+        self.assertEqual(self.dll.head.next.next.data, 30)
+        self.assertEqual(self.dll.tail.prev.prev.data, 10)
+
+    def test_insert_multiple_times_head(self):
+        # Insert multiple values sequentially
+        for i in range(1, 6):
+            self.dll.insert_at_head(i * 10)
+        current = self.dll.head
+        expected_values = [50, 40, 30, 20, 10]
+        for value in expected_values:
+            self.assertEqual(current.data, value)
+            current = current.next
+        self.assertEqual(self.dll.tail.data, 10)
         self.assertEqual(self.dll.count, 5)
 
 
