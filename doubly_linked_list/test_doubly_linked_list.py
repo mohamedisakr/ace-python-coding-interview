9ffce0458cd423155fa765c79abd2bf618d7fa85
mohamedisakr <mohamedisakr@gmail.com>
2024-10-12 12:01:33 +0300

    doubly linked list delete test cases

--- doubly_linked_list/test_doubly_linked_list.py
+++ doubly_linked_list/test_doubly_linked_list.py
@@ -91,6 +91,65 @@ class TestDoublyLinkedList(unittest.TestCase):
         self.assertEqual(self.dll.tail.data, 10)
         self.assertEqual(self.dll.count, 5)
 
+    # ---- delete -----
+
+    def test_empty_list_delete(self):
+        # Delete from an empty list
+        self.dll.delete(10)
+        self.assertIsNone(self.dll.head)
+        self.assertIsNone(self.dll.tail)
+
+    def test_single_node_list_delete(self):
+        # Delete the only node in the list
+        self.dll.insert_at_tail(10)
+        self.dll.delete(10)
+        self.assertIsNone(self.dll.head)
+        self.assertIsNone(self.dll.tail)
+
+    def test_delete_head(self):
+        # Delete the head node in a list with multiple nodes
+        self.dll.insert_at_tail(10)
+        self.dll.insert_at_tail(20)
+        self.dll.insert_at_tail(30)
+        self.dll.delete(10)
+        self.assertEqual(self.dll.head.data, 20)
+        self.assertEqual(self.dll.head.next.data, 30)
+        self.assertIsNone(self.dll.head.prev)
+        self.assertEqual(self.dll.tail.data, 30)
+
+    def test_delete_tail(self):
+        # Delete the tail node in a list with multiple nodes
+        self.dll.insert_at_tail(10)
+        self.dll.insert_at_tail(20)
+        self.dll.insert_at_tail(30)
+        self.dll.delete(30)
+        self.assertEqual(self.dll.tail.data, 20)
+        self.assertEqual(self.dll.tail.prev.data, 10)
+        self.assertIsNone(self.dll.tail.next)
+        self.assertEqual(self.dll.head.data, 10)
+
+    def test_delete_middle_node(self):
+        # Delete a node that is neither head nor tail in a list with multiple nodes
+        self.dll.insert_at_tail(10)
+        self.dll.insert_at_tail(20)
+        self.dll.insert_at_tail(30)
+        self.dll.insert_at_tail(40)
+        self.dll.delete(20)
+        self.assertEqual(self.dll.head.data, 10)
+        self.assertEqual(self.dll.head.next.data, 30)
+        self.assertEqual(self.dll.head.next.prev.data, 10)
+        self.assertEqual(self.dll.tail.data, 40)
+
+    def test_delete_non_existent_node(self):
+        # Attempt to delete a value that does not exist in the list
+        self.dll.insert_at_tail(10)
+        self.dll.insert_at_tail(20)
+        self.dll.insert_at_tail(30)
+        self.dll.delete(40)
+        self.assertEqual(self.dll.head.data, 10)
+        self.assertEqual(self.dll.tail.data, 30)
+        self.assertEqual(self.dll.count, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
