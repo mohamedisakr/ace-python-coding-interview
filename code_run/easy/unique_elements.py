18dc786223ba07a745c14caf1ad3f053bf1c434a
mohamedisakr <mohamedisakr@gmail.com>
2024-10-19 16:42:39 +0300

    unique elements revise

--- code_run/easy/unique_elements.py
+++ code_run/easy/unique_elements.py
@@ -8,15 +8,6 @@ def main():
     arr = list(map(int, input().split()))
     uniq = Counter(arr)
     print(sum(1 for x in uniq.values() if x == 1))
-    # n = int(input())
-    # arr = list(map(int, input().split()))
-    # uniq = {}
-    # for item in arr:
-    #     if item not in uniq:
-    #         uniq[item] = True
-    #     else:
-    #         uniq[item] = False
-    # print(sum(1 for value in uniq.values() if value is True))
 
 
 if __name__ == '__main__':
