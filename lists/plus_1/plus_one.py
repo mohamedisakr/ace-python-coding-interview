ae865a8de9cccfca75c4a94a08ed2730d0749935
mohamedisakr <mohamedisakr@gmail.com>
2024-11-01 06:32:28 +0200

    plus one impl

---
+++ lists/plus_1/plus_one.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1]+digits
+
+    # def plusOne(self, digits: List[int]) -> List[int]:
+    #     result = int(''.join(map(str, digits)))
+    #     result += 1
+    #     arr = [int(digit) for digit in str(result)]
+    #     return arr
+
+
+digits = [9]  # [4, 3, 2, 1]  # [1, 2, 3]
+caller = Solution()
+num = caller.plusOne(digits)
+print(num)
