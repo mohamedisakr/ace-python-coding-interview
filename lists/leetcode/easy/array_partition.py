2097bb9151cc566b5be24691fde02a1b261a0a4d
mohamedisakr <mohamedisakr@gmail.com>
2024-12-02 06:00:52 +0200

    array partition

---
+++ lists/leetcode/easy/array_partition.py
@@ -0,0 +1,9 @@
+# 561. Array Partition
+# https://leetcode.com/problems/array-partition/description/
+
+from typing import List
+
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        return sum(sorted(nums)[::2])
