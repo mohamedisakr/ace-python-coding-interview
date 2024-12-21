c8117fceb3f10da4f27dcccf2eaa646121174429
mohamedisakr <mohamedisakr@gmail.com>
2024-12-21 16:51:55 +0200

    build array from permutation

---
+++ simulation/easy/build_array_from_permutation.py
@@ -0,0 +1,29 @@
+# 1920. Build Array from Permutation
+# https://leetcode.com/problems/build-array-from-permutation/description/
+
+from typing import List
+
+
+class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+
+        for i, num in enumerate(nums):
+            nums[i] += n * (nums[num] % n)
+
+        for i in range(n):
+            nums[i] //= n
+
+        return nums
+
+# class Solution:
+#     def buildArray(self, nums: List[int]) -> List[int]:
+#         return [nums[num] for num in nums]
+
+# class Solution:
+#     def buildArray(self, nums: List[int]) -> List[int]:
+#         n = len(nums)
+#         ans = n * 0
+#         for i in range(n):
+#             ans[i] = nums[nums[i]]
+#         return ans
