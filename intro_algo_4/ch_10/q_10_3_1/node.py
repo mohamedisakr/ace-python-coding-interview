60e6c8bd00b62357504ec623fc28c14ba3490988
mohamedisakr <mohamedisakr@gmail.com>
2024-12-30 20:32:01 +0200

    tree initialization

--- intro_algo_4/ch_10/q_10_3_1/node.py
+++ intro_algo_4/ch_10/q_10_3_1/node.py
@@ -1,8 +1,7 @@
-from typing import Optional
+from typing import Dict, Optional
 
 
 class TreeNode:
-    # left: int, right: int
     def __init__(self, key: int, index: Optional[int] = None) -> None:
         self._key: int = key
         self._index: Optional[int] = index
@@ -12,4 +11,15 @@ class TreeNode:
 
 class BinaryTree:
     def __init__(self) -> None:
-        self._root = None
+        self._root: Optional[TreeNode] = None
+        self._nodes: Dict[int, TreeNode] = None
+
+    def inorder(self) -> None:
+        def _inorder(node: Optional[TreeNode]) -> None:
+            if node:
+                self._inorder(node.left)
+                print(f"({node._index}) {node._key}")
+                self._inorder(node._right)
+
+        if self._root:
+            _inorder(self._root)
