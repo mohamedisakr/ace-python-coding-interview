bc5ea8d03241248bf4f33fbb47503533c6f7d7a5
mohamedisakr <mohamedisakr@gmail.com>
2025-01-17 23:18:01 +0200

    type of triangle

---
+++ maths/leetcode/easy/type_of_triangle.py
@@ -0,0 +1,18 @@
+# 3024. Type of Triangle
+# https://leetcode.com/problems/type-of-triangle/description
+from typing import List
+
+
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        nums.sort()
+        if nums[0] + nums[1] <= nums[2]:
+            return "none"
+
+        if nums[0] == nums[1] == nums[2]:
+            return "equilateral"
+
+        if nums[0] == nums[1] or nums[1] == nums[2]:
+            return "isosceles"
+
+        return "scalene"
