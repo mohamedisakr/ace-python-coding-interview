31ccb431e463e84c1259823d3a42c9fb103d1fff
mohamedisakr <mohamedisakr@gmail.com>
2025-02-04 00:19:19 +0200

    coin change

--- dynamic_programming/medium/coin_change.py
+++ dynamic_programming/medium/coin_change.py
@@ -6,4 +6,10 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        pass
+        dp = [0] + [float('inf')] * amount
+        for curr_amount in range(1, amount+1):
+            for coin in coins:
+                if curr_amount - coin >= 0:
+                    dp[curr_amount] = min(
+                        dp[curr_amount], 1 + dp[curr_amount - coin])
+        return dp[amount] if dp[amount] != float('inf') else -1
