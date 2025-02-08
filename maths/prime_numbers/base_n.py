c5162e03b409233d8793e18a1f94980d349ab6e8
mohamedisakr <mohamedisakr@gmail.com>
2025-02-08 09:28:32 +0200

    hex to decimal

--- maths/prime_numbers/base_n.py
+++ maths/prime_numbers/base_n.py
@@ -9,9 +9,27 @@ def oct_to_decimal(num):
     return decimal
 
 
-base_8 = [23, 62, 471, 266]
-for num in base_8:
-    print(f'{num} in base 8 is {oct_to_decimal(num)} in base 10')
+def hex_to_decimal(s):
+    letter_map = {'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    exp = 0
+    decimal = 0
+    for char in s[::-1]:
+        # 'DA6'
+        # case 1: letter
+        if char in letter_map:
+            decimal += letter_map[char] * (16 ** exp)
+        # case 2: number
+        else:
+            decimal += int(char) * (8 ** exp)
+        exp += 1
+    return decimal
 
-# num = 23
-# print(oct_to_decimal(num))
+
+# base_8 = [23, 62, 471, 266]
+# for num in base_8:
+#     print(f'{num} in base 8 is {oct_to_decimal(num)} in base 10')
+
+
+base_16 = ['286', '591', '4A', 'C7', 'DA6', '1B4']
+for num in base_16:
+    print(f'{num} in base 16 is {hex_to_decimal(num)} in base 10')
