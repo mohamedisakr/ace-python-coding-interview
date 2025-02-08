2d8326ccdc2c69cfacd852c23f50d0b919c4b332
mohamedisakr <mohamedisakr@gmail.com>
2025-02-08 14:45:13 +0200

    base 10 to base 8

--- maths/prime_numbers/decimals.py
+++ maths/prime_numbers/decimals.py
@@ -10,8 +10,21 @@ def to_7(num: int) -> int:
     return int(base_7)
 
 
-num = 862
-print(to_7(num))
+def to_8(num: int) -> int:
+    if num == 0:
+        return 0
+    base_8 = ''
+    while num:
+        base_8 = str(num % 8) + base_8
+        num //= 8
+    return int(base_8)
+
+
+# num = 862
+# print(to_7(num))
+
+num = 129
+print(to_8(num))
 
 # def to_7(num: int):  # -> int:
 #     b = 7
