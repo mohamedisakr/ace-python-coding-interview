6ec07204a98ab3b83a198143bbbb7ed858d313cb
mohamedisakr <mohamedisakr@gmail.com>
2025-05-01 14:23:43 +0200

    maximum product of two elements - revised

--- sorting/leetcode/easy/maximum_product_of_two_elements.py
+++ sorting/leetcode/easy/maximum_product_of_two_elements.py
@@ -3,5 +3,16 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        nums.sort()
-        return (nums[-1] - 1) * (nums[-2] - 1)
+        max1 = 0
+        max2 = 0
+
+        for num in nums:
+            if num > max1:
+                max2, max1 = max1, num
+            elif num > max2:
+                max2 = num
+
+        return (max1 - 1) * (max2 - 1)
+
+        # nums.sort()
+        # return (nums[-1] - 1) * (nums[-2] - 1)
