fdff5c8eef789244a313622fb40edef2094e3f6c
mohamedisakr <mohamedisakr@gmail.com>
2025-05-10 08:04:10 +0200

    TODO: matrix cells in distance order

---
+++ sorting/leetcode/easy/matrix_cells_in_distance_order.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+        # TODO uses a BFS (Breadth-First Search)
