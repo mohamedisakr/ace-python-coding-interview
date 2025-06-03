6697b4e909e6c7a759604ffc86161417e859a8dd
mohamedisakr <mohamedisakr@gmail.com>
2025-06-03 17:52:14 +0200

    greatest common divisor

--- maths/prime_numbers/gcd.py
+++ maths/prime_numbers/gcd.py
@@ -8,16 +8,26 @@ def is_coprime(a: int, b: int) -> bool:
     return gcd(a, b) == 1
 
 
-# cases = [
-#     [35, 12],
-#     [122, 39],
-#     [63, 42],
-#     [320, 80],
-#     [462, 200]]
+cases = [
+    [30, 96],
+    [18, 24],
+    [360, 48],
+    [27, 39],
+    [100, 63],
+    [500, 300],
+    [70, 42],
+    [4, 9],
+    [10, 27],
+    [606060, 707070],
+    [35, 12],
+    [122, 39],
+    [63, 42],
+    [320, 80],
+    [462, 200]]
 
-# for a, b in cases:
-#     print(gcd(a, b))
+for a, b in cases:
+    print(f'{a} and {b}: {gcd(a, b)}')  # The GCD for # \t\t\t\t\t
 
-# (a) Use the Euclidean algorithm to show that 68 and 345 are relatively prime
-a, b = 68, 345
-print(f'{a} and {b} are relatively prime {is_coprime(a, b)}')
+# # (a) Use the Euclidean algorithm to show that 68 and 345 are relatively prime
+# a, b = 68, 345
+# print(f'{a} and {b} are relatively prime {is_coprime(a, b)}')
