55816869c9ff25184933876a49336fca2a984908
mohamedisakr <mohamedisakr@gmail.com>
2025-09-25 15:51:26 +0300

    quicksort iterative adding randomized pivot selection

--- sorting/quicksort/quicksort_iterative.py
+++ sorting/quicksort/quicksort_iterative.py
@@ -1,15 +1,21 @@
-def quicksort_iterative(arr):
+from random import randint
+from typing import List
+
+
+def quicksort_iterative(arr: List[int]) -> None:
     stack = [(0, len(arr) - 1)]
 
     while stack:
         low, high = stack.pop()
         if low < high:
-            pivot_index = partition(arr, low, high)
+            pivot_index = randomized_partition(arr, low, high)
             stack.append((low, pivot_index - 1))
             stack.append((pivot_index + 1, high))
 
 
-def partition(arr, low, high):
+def randomized_partition(arr: List[int], low: int, high: int) -> int:
+    rand_index = randint(low, high)
+    arr[high], arr[rand_index] = arr[rand_index], arr[high]  # swap pivot to end
     pivot = arr[high]
     i = low
     for j in range(low, high):
