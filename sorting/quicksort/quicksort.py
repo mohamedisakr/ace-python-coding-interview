d4f5a7969fdfc453c32c6d280123a275ea078774
mohamedisakr <mohamedisakr@gmail.com>
2025-09-25 14:06:46 +0300

    quicksort in-place using Lomuto partitioning code

---
+++ sorting/quicksort/quicksort.py
@@ -0,0 +1,18 @@
+# Lomuto Partition Scheme (In-Place)
+
+def quicksort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quicksort(arr, low, pivot_index - 1)
+        quicksort(arr, pivot_index + 1, high)
+
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low  # boundary for elements < pivot
+    for j in range(low, high):
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
