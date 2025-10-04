ff284ee0db4552b3ed3a6f1eef8101e5748af003
mohamedisakr <mohamedisakr@gmail.com>
2025-10-04 11:30:50 +0300

    prime factorization

---
+++ maths/prime_numbers/prime_factor.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def factor(num: int) -> List[int]:
+    primes = []
+    while num % 2 == 0:
+        primes.append(2)
+        num //= 2
+
+    for p in range(3, int(num ** 0.5)+1, 2):
+        while num % p == 0:
+            primes.append(p)
+            num //= p
+
+    if num > 1:
+        primes.append(num)
+
+    return primes
+
+
+num = 130
+primes = factor(num)
+print(primes)
