31efa73f5872f90e0ac3093d1e28ea6cf09f526a
mohamedisakr <mohamedisakr@gmail.com>
2025-10-10 10:03:09 +0300

    bubble sort

---
+++ sorting/bubble/intro.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def sort(nums: List[int]):
+    n = len(nums)
+    for i in range(n):
+        swapped = False
+        for j in range(n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                swapped = True
+
+        if not swapped:
+            break
+    return nums
+
+
+nums = [5, 1, 4, 2, 8]
+print(sort(nums))
