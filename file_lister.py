085c95d6cf5446da829446ae823c5271fd8749e6
mohamedisakr <mohamedisakr@gmail.com>
2025-12-12 07:31:51 +0200

    file lister remove extension

--- file_lister.py
+++ file_lister.py
@@ -1,14 +1,16 @@
 from os import listdir
 
-directory_path = r"J:\Videos\System Design\00 AlgoExpert Systems Design Fundamentals"
+directory_path = r"J:\Videos\Numerical Analysis - Root Finding\Youtube Root Finding (Oscar Veliz)"
 files = listdir(directory_path)
 print(f'# of files: {len(files)}')
-file_names = [file.replace(".mp4", "") for file in files if not ".1." in file]
+file_names = [fname for fname in files if fname.endswith('.mp4')]
+#[file.replace(".mp4", "") for file in files if not ".1." in file]
 #
 print(f'# of files after processed: {len(file_names)}')
 # .split('.')[-2].rstrip()file_names]
-
+# files = [fname for fname in allfiles if fname.endswith('.mp4')]
 with open('files-list.txt', 'w', encoding="utf-8") as file:
     for i, v in enumerate((file_names)):
         # Write some text to the file
+        print(file_names[i][0:-4])
         file.write(f'{v}\n')
