e5a22ef2bb4ee8fd73627d3a0d34fea084ca4027
mohamedisakr <mohamedisakr@gmail.com>
2025-12-31 07:56:12 +0200

    geometric progression nth term

---
+++ geometric_progression/nth_term.py
@@ -0,0 +1,18 @@
+def find_nth_term_from_series(series, n):
+    """
+    Calculates the n-th term of a GP based on a provided list.
+    """
+    if len(series) < 2:
+        return "The series needs at least two numbers to determine the ratio."
+
+    a = series[0]
+    r = series[1] / a
+    return a * (r ** (n-1))
+
+
+my_series = [5, 10, 20, 40]
+target_n = 10
+
+result = find_nth_term_from_series(my_series, target_n)
+
+print(f"For the series {my_series}, the {target_n}th term is: {result}")
