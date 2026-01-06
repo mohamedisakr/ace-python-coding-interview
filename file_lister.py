5a19a91fa1aca1eb7e2c92557d9d9ae58b55de14
mohamedisakr <mohamedisakr@gmail.com>
2026-01-06 07:27:54 +0200

    file list

--- file_lister.py
+++ file_lister.py
@@ -1,10 +1,10 @@
 from os import listdir
 
-directory_path = r"J:\Videos\Numerical Analysis - Root Finding\Youtube Root Finding (Oscar Veliz)"
+directory_path = r"J:\Videos\Udemy\Udemy The Ultimate Unofficial Udemy Online Course Creation Guide 2024-8"
 files = listdir(directory_path)
 print(f'# of files: {len(files)}')
-file_names = [fname for fname in files if fname.endswith('.mp4')]
-#[file.replace(".mp4", "") for file in files if not ".1." in file]
+file_names = [fname for fname in files]  # if fname.endswith('.mp4')]
+# [file.replace(".mp4", "") for file in files if not ".1." in file]
 #
 print(f'# of files after processed: {len(file_names)}')
 # .split('.')[-2].rstrip()file_names]
@@ -12,5 +12,6 @@ print(f'# of files after processed: {len(file_names)}')
 with open('files-list.txt', 'w', encoding="utf-8") as file:
     for i, v in enumerate((file_names)):
         # Write some text to the file
-        print(file_names[i][0:-4])
+        # print(file_names[i][0:-4])
+        print(file_names[i])
         file.write(f'{v}\n')
