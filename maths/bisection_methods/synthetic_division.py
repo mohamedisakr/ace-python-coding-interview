208917cbf85d7a1d06d3498684420619b0df27a6
mohamedisakr <mohamedisakr@gmail.com>
2026-01-14 14:53:22 +0200

    synthetic division

---
+++ maths/bisection_methods/synthetic_division.py
@@ -0,0 +1,32 @@
+def synthetic_division(coefficients, c):
+    """
+    Performs synthetic division of a polynomial by (x - c).
+    :param coefficients: List of ints/floats (e.g., [1, -4, 0, 6] for x^3 - 4x^2 + 6)
+    :param c: The root/value to divide by
+    :return: (quotient coefficients, remainder)
+    """
+    # The first coefficient is always brought down as-is
+    new_coeffs = [coefficients[0]]
+
+    # Iterate through the remaining coefficients
+    for i in range(1, len(coefficients)):
+        # Multiply the previous result by 'c' and add the current coefficient
+        next_val = (new_coeffs[-1] * c) + coefficients[i]
+        new_coeffs.append(next_val)
+
+    # The last value in the list is the remainder
+    remainder = new_coeffs.pop()
+    quotient = new_coeffs
+
+    return quotient, remainder
+
+
+# Example: (2x^3 + 3x^2 - 8x + 3) / (x - 1)
+# Coefficients: [2, 3, -8, 3], c = 1
+coeffs = [2, 3, -8, 3]
+root = 1
+
+q, r = synthetic_division(coeffs, root)
+
+print(f"Quotient Coefficients: {q}")
+print(f"Remainder: {r}")
