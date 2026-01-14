502625ae70da6c102cfa18e2320730cd7d63f6d0
mohamedisakr <mohamedisakr@gmail.com>
2026-01-14 15:47:17 +0200

    synthetic division

--- maths/bisection_methods/synthetic_division.py
+++ maths/bisection_methods/synthetic_division.py
@@ -21,12 +21,33 @@ def synthetic_division(coefficients, c):
     return quotient, remainder
 
 
+# # Example: (2x^3 + 3x^2 - 8x + 3) / (x - 1)
+# # Coefficients: [2, 3, -8, 3], c = 1
+# coeffs = [2, 3, -8, 3]
+# root = 1
+
+# q, r = synthetic_division(coeffs, root)
+
+# print(f"Quotient Coefficients: {q}")
+# print(f"Remainder: {r}")
+
+
+def my_synthetic_division(coeffs, root):
+    n = len(coeffs)
+    res_cofeffs = [coeffs[0]]
+    for i in range(1, n):
+        next_val = (res_cofeffs[-1] * root) + coeffs[i]
+        res_cofeffs.append(next_val)
+    remainder = res_cofeffs.pop()
+    return res_cofeffs, remainder
+
+
 # Example: (2x^3 + 3x^2 - 8x + 3) / (x - 1)
 # Coefficients: [2, 3, -8, 3], c = 1
-coeffs = [2, 3, -8, 3]
+coeffs = [2, 3, -8, 3]  # will return [2, 5, -3]
 root = 1
 
-q, r = synthetic_division(coeffs, root)
+q, r = my_synthetic_division(coeffs, root)
 
 print(f"Quotient Coefficients: {q}")
 print(f"Remainder: {r}")
